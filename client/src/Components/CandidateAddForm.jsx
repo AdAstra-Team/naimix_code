06951f759e9bd871cc11b0e2c2dffc6f5271da73
Savr123 <Sangievv@mail.ru>
2024-11-17 01:35:29 +0300

    хуйня

--- client/src/Components/CandidateAddForm.jsx
+++ client/src/Components/CandidateAddForm.jsx
@@ -40,7 +40,7 @@ const CandidateAddForm = ({ onClose }) => {
 
     onClose();
 
-    console.log(formData);
+    console.log(response.data);
   };
 
   return (
@@ -131,6 +131,28 @@ const CandidateAddForm = ({ onClose }) => {
             {SIGNS.map((item) => (<option>{item}</option>))}
           </select>
         </div>
+
+        <div className="mb-4">
+          <label className="block text-gray-700 mb-2" htmlFor="image">
+            Картинка
+          </label>
+          <input
+            className="w-full border border-gray-300 rounded-lg p-2"
+            type="file"
+            id="image"
+            name="image"
+            accept="image/*"
+            onChange={handleChange}
+          />
+          {formData.previewImageUrl && (
+            <img
+              src={formData.previewImageUrl}
+              alt="Preview"
+              className="mt-2 w-full object-cover h-48"
+            />
+          )}
+        </div>
+
         <div className="flex justify-center">
           <button className="bg-orange-500 rounded-lg px-4 py-2" type="submit">
             Добавить кандидата
