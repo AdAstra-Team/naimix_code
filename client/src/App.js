56f63caa18e070cf9481167d69bbfa1296733d32
Savr123 <Sangievv@mail.ru>
2024-11-16 04:12:31 +0300

    роуты

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useSelector } from 'react-redux';
+import { BrowserRouter as Router, Routes, Route, useNavigate, redirect } from 'react-router-dom';
 
 function App() {
 
@@ -8,20 +9,10 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+          <Routes>
+          </Routes>
+      </Router>
     </div>
   );
 }
