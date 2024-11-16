64776df8107ff81f55d87c389cc6e59a874c81c8
Alexander Afonin <63101279+lueFlake@users.noreply.github.com>
2024-11-16 22:11:33 +0300

    fixed teams and candidates

--- server/Elongation/projects/src/main/java/org/example/models/dao/TeamRequest.java
+++ server/Elongation/projects/src/main/java/org/example/models/dao/TeamRequest.java
@@ -1,4 +1,34 @@
 package org.example.models.dao;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class TeamRequest {
+    private String name;
+    private ArrayList<Integer> signs;
+    private UUID recruiterId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Integer> getSigns() {
+        return signs;
+    }
+
+    public void setSigns(ArrayList<Integer> signs) {
+        this.signs = signs;
+    }
+
+    public UUID getRecruiterId() {
+        return recruiterId;
+    }
+
+    public void setRecruiterId(UUID recruiterId) {
+        this.recruiterId = recruiterId;
+    }
 }
