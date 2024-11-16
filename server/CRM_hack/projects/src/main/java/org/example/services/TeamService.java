4e950d648b4299848d6085510a4d6faea858e2b9
Alexander Afonin <63101279+lueFlake@users.noreply.github.com>
2024-11-16 19:20:24 +0300

    added Service attr

--- server/CRM_hack/projects/src/main/java/org/example/services/TeamService.java
+++ server/CRM_hack/projects/src/main/java/org/example/services/TeamService.java
@@ -3,10 +3,12 @@ package org.example.services;
 import org.example.models.dao.TeamRequest;
 import org.example.models.dao.TeamResponse;
 import org.example.models.entities.Team;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public class TeamService {
     public List<Team> getAllTasks() {
         return null;
