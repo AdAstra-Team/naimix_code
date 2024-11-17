280ff2edd3a377611b9bf41bbdc7092ca5087e15
Savr123 <Sangievv@mail.ru>
2024-11-17 09:27:49 +0300

    увеличил iframe

--- client/src/Pages/Metrics.jsx
+++ client/src/Pages/Metrics.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Metrics() {
     return (
-      <div style={{ border: "1px solid #ccc", padding: "10px", height: "500px" }}>
-        <iframe
+      <div className="h-screen" style={{ border: "1px solid #ccc", padding: "10px"}}>
+        <iframe className="h-screen"
           src="http://detulie.space/stat/public-dashboards/c33cba9873c04003ba28abf14e0ec2f9"
           title="Embedded Site"
           style={{
